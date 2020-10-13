53067d6f51d3feb5642f20cde54a3bb312a68d63
Seb-Alonso <sebacg167@gmail.com>
2020-10-13 16:44:27 -0300

    primera actualizacion

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
+import ejercicio1 from './modules/ejercicio1.js';
+
 caja = document.getElementById("caja");
+
+document.getElementById("ej1").addEventListener("click",()=>{
+    caja.innerHTML = ejercicio1.data;
+});
---
+++ js/modules/ejercicio1.js
@@ -0,0 +1,22 @@
+export default{
+    puntajes: [{alumno:'Pablo Rojas',puntaje:9,resultado:'BUENO'}],
+    data:html`
+        <h1>Autor: Sebastian Chamorro</h1>
+        alumno<br/>
+        <input id="nombre" type="text"><br/>
+        puntaje<br/>
+        <select id="puntaje">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+        </select>
+    `,
+
+}
