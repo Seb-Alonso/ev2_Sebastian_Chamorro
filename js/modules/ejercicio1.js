466d2faa34b1a28d5d44b4028584ba3568ce550a
Seb-Alonso <sebacg167@gmail.com>
2020-10-13 17:10:52 -0300

    actualizacion 2

--- js/modules/ejercicio1.js
+++ js/modules/ejercicio1.js
@@ -1,11 +1,12 @@
 export default{
     puntajes: [{alumno:'Pablo Rojas',puntaje:9,resultado:'BUENO'}],
-    data:html`
+    data:`
         <h1>Autor: Sebastian Chamorro</h1>
         alumno<br/>
         <input id="nombre" type="text"><br/>
         puntaje<br/>
         <select id="puntaje">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -16,7 +17,37 @@ export default{
             <option value="8">8</option>
             <option value="9">9</option>
             <option value="10">10</option>
-        </select>
+        </select><br/>
+        <button>registrar</button><br/>
+        <div id="salida"></div>
+
     `,
+    cargar:function(){
+        var salida = document.getElementById(salida);
+        var tabla =`
+        <table>
+            <tr>
+                <th>alumno</th>
+                <th>puntaje</th>
+                <th>resultado</th>
+            </tr>
+        
+        `;
+        this.puntajes.forEach((item)=>{
+            tabla +=`
+                <tr>
+                    <th>${item.alumno}</th>
+                    <th>${item.puntaje}</th>
+                    <th>${item.resultado}</th>
+                </tr>
+        `
+        });
+        tabla += '</table>';
+        salida.innerHTML = tabla;
+        
+    },
+    registar:function(){
+
+    },
 
 }
